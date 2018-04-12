5e2832ce7296e7da788e5e0bb9e7dda151403b75
Reynaldo Teixeira <reynaldoteixeira@gmail.com>
2018-04-12 16:41:08 -0300

    Sections funcionando com JSON (angular)

--- public/js/controller/potions-controller.js
+++ public/js/controller/potions-controller.js
@@ -1,5 +1,15 @@
 angular.module('myApp').controller('PotionsController',function($scope,$http){
 
-	
+	$scope.importar = function() {
+
+		$http.get('potions.json').success(function(dados){ //acessando os dados do arquivo Json
+
+			$scope.pocoes=dados;
+
+		});
+
+	}
+
+	$scope.importar();
 
 });
